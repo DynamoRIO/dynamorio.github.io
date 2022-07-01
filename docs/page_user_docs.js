e456a0b66f7f7547469c7ffcf7ec37032c3a22f2
cronbuild <dynamorio-devs@googlegroups.com>
2022-07-01 04:19:23 +0000

    Snapshot for cronbuild-9.0.19172

--- docs/page_user_docs.js
+++ docs/page_user_docs.js
@@ -91,7 +91,7 @@
       [ "DynamoRIO Shared Library Issues", "page_standalone.html#sec_standalone_shared", null ]
     ] ],
     [ "DynamoRIO System Overview", "overview.html", {% include_relative overview.js %} ],
-    [ "Release Notes for Version 9.0.19165", "release_notes.html", [
+    [ "Release Notes for Version 9.0.19172", "release_notes.html", [
       [ "Distribution Contents", "release_notes.html#sec_package", null ],
       [ "Changes Since Prior Releases", "release_notes.html#sec_changes", null ],
       [ "Limitations", "release_notes.html#sec_limits", [
