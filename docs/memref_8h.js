53d5748d37401e9de839424bedab878c329488a2
cronbuild <dynamorio-devs@googlegroups.com>
2023-08-04 03:19:13 +0000

    Snapshot for cronbuild-9.93.19572

--- docs/memref_8h.js
+++ docs/memref_8h.js
@@ -5,7 +5,7 @@
     [ "_memref_thread_exit_t", "structdynamorio_1_1drmemtrace_1_1__memref__thread__exit__t.html", {% include_relative structdynamorio_1_1drmemtrace_1_1__memref__thread__exit__t.js %} ],
     [ "_memref_marker_t", "structdynamorio_1_1drmemtrace_1_1__memref__marker__t.html", {% include_relative structdynamorio_1_1drmemtrace_1_1__memref__marker__t.js %} ],
     [ "_memref_t", "uniondynamorio_1_1drmemtrace_1_1__memref__t.html", {% include_relative uniondynamorio_1_1drmemtrace_1_1__memref__t.js %} ],
-    [ "memref_pid_t", "memref_8h.html#af3c564e046699e468f4067c1a5d3977b", null ],
+    [ "memref_pid_t", "memref_8h.html#a513b2e3221fb3f740542d7498ab25fa8", null ],
     [ "memref_t", "memref_8h.html#ae4a471ed806967b2b6f911b6ef770704", null ],
-    [ "memref_tid_t", "memref_8h.html#ac8cf8426ccc05b762f30a1a4b07f8431", null ]
+    [ "memref_tid_t", "memref_8h.html#a3e5e148e77271ee047c9f3e79f82fd87", null ]
 ],
