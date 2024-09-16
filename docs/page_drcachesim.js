bb0689ffbca6ec42639007ed447e0b7a44b28fc0
cronbuild <dynamorio-devs@googlegroups.com>
2024-09-16 20:33:58 +0000

    Snapshot for cronbuild-10.93.19982

--- docs/page_drcachesim.js
+++ docs/page_drcachesim.js
@@ -5,7 +5,7 @@
       [ "Memory Access Records", "sec_drcachesim_format.html#sec_drcachesim_format_data", null ],
       [ "Other Records", "sec_drcachesim_format.html#sec_drcachesim_format_other", null ]
     ] ],
-    [ "Running the Cache Simulator", "sec_drcachesim_run.html", null ],
+    [ "Running Tools", "sec_drcachesim_run.html", null ],
     [ "Analysis Tool Suite", "sec_drcachesim_tools.html", [
       [ "Cache Simulator", "sec_drcachesim_tools.html#sec_tool_cache_sim", null ],
       [ "TLB Simulator", "sec_drcachesim_tools.html#sec_tool_TLB_sim", null ],
@@ -34,14 +34,23 @@
     [ "Cache Miss Analyzer", "sec_drcachesim_analyzer.html", null ],
     [ "Physical Addresses", "sec_drcachesim_phys.html", null ],
     [ "Core Simulation Support", "sec_drcachesim_core.html", null ],
+    [ "Trace Scheduler", "sec_drcachesim_sched.html", [
+      [ "As-Traced Schedule Limitations", "sec_drcachesim_sched.html#sec_drcachesim_as_traced", null ],
+      [ "Dynamic Scheduling", "sec_drcachesim_sched.html#sec_drcachesim_sched_dynamic", null ],
+      [ "Simulated Time", "sec_drcachesim_sched.html#sec_drcachesim_sched_time", null ],
+      [ "Idle Time", "sec_drcachesim_sched.html#sec_drcachesim_sched_idle", null ],
+      [ "Record and Replay", "sec_drcachesim_sched.html#sec_drcachesim_sched_replay", null ],
+      [ "Regions of Interest", "sec_drcachesim_sched.html#sec_drcachesim_sched_roi", null ],
+      [ "Speculation Support", "sec_drcachesim_sched.html#sec_drcachesim_sched_speculation", null ],
+      [ "Scheduler Interface Example", "sec_drcachesim_sched.html#sec_drcachesim_sched_ex", null ]
+    ] ],
     [ "Extending the Simulator", "sec_drcachesim_extend.html", null ],
     [ "Customizing the Tracer", "sec_drcachesim_tracer.html", null ],
     [ "Tracing Function Calls", "sec_drcachesim_funcs.html", null ],
     [ "Creating New Analysis Tools", "sec_drcachesim_newtool.html", [
       [ "Separately-Built Tools", "sec_drcachesim_newtool.html#external_tools", [
         [ "External tool package", "sec_drcachesim_newtool.html#autotoc_md351", null ]
-      ] ],
-      [ "Scheduler", "sec_drcachesim_newtool.html#sec_drcachesim_sched", null ]
+      ] ]
     ] ],
     [ "Simulator Parameters", "sec_drcachesim_ops.html", null ],
     [ "Current Limitations", "sec_drcachesim_limit.html", null ],
