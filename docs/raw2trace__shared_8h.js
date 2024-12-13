8afbefad5c71e66aef37a13efc592bc976467079
cronbuild <dynamorio-devs@googlegroups.com>
2024-12-13 03:42:10 +0000

    Snapshot for cronbuild-11.0.20069

--- docs/raw2trace__shared_8h.js
+++ docs/raw2trace__shared_8h.js
@@ -1,3 +1,5 @@
 [
-    [ "trace_metadata_reader_t", "structdynamorio_1_1drmemtrace_1_1trace__metadata__reader__t.html", null ]
+    [ "trace_metadata_reader_t", "structdynamorio_1_1drmemtrace_1_1trace__metadata__reader__t.html", null ],
+    [ "module_mapper_t", "classdynamorio_1_1drmemtrace_1_1module__mapper__t.html", {% include_relative classdynamorio_1_1drmemtrace_1_1module__mapper__t.js %} ],
+    [ "test_module_mapper_t", "classdynamorio_1_1drmemtrace_1_1test__module__mapper__t.html", null ]
 ],
