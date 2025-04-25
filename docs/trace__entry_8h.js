cef7b347daf947f96d09bde7d71b690f1d1c37e4
cronbuild <dynamorio-devs@googlegroups.com>
2025-04-25 03:44:39 +0000

    Snapshot for cronbuild-11.90.20201

--- docs/trace__entry_8h.js
+++ docs/trace__entry_8h.js
@@ -69,6 +69,7 @@
       [ "TRACE_MARKER_TYPE_MAYBE_BLOCKING_SYSCALL", "trace__entry_8h.html#ab8531c7f66ccc0d7fe59f436844a295caa6544f9f8c54496a7befa481c81577b7", null ],
       [ "TRACE_MARKER_TYPE_SYSCALL_TRACE_START", "trace__entry_8h.html#ab8531c7f66ccc0d7fe59f436844a295ca13fffec3c874dcd5cc84aa25387b76d3", null ],
       [ "TRACE_MARKER_TYPE_SYSCALL_TRACE_END", "trace__entry_8h.html#ab8531c7f66ccc0d7fe59f436844a295caef6df6ddbdffd994f91edba51c850cde", null ],
+      [ "TRACE_MARKER_TYPE_BRANCH_TARGET", "trace__entry_8h.html#ab8531c7f66ccc0d7fe59f436844a295ca9ccf81370a91222945dc5d3e44fb718d", null ],
       [ "TRACE_MARKER_TYPE_SYSCALL_FAILED", "trace__entry_8h.html#ab8531c7f66ccc0d7fe59f436844a295cac4123c88a189fe7b52ceee6b7cab48ce", null ],
       [ "TRACE_MARKER_TYPE_DIRECT_THREAD_SWITCH", "trace__entry_8h.html#ab8531c7f66ccc0d7fe59f436844a295cae7bcf6a915571421473532017fc86d24", null ],
       [ "TRACE_MARKER_TYPE_CORE_WAIT", "trace__entry_8h.html#ab8531c7f66ccc0d7fe59f436844a295ca8d688aeae7d50e45269dbb8b494cbac1", null ],
